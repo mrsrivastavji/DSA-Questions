bc8a78e4f8e866c4fe1419f61ceefcc412b3338d
Shivam Srivastav <107093652+mrsrivastavji@users.noreply.github.com>
2025-06-20 12:07:37 +0530

    Create arrayList.java

---
+++ arrayList.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class ArrayLis {
+    public static void main(String args[]){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        
+        list.add(0);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        System.out.println(list);
+        
+        int element = list.get(0);
+        System.out.println(element);
+        
+        list.add(0,5);
+        System.out.println(list);
+        
+        list.set(0,0);
+        list.set(1,4);
+        list.set(2,2);
+        list.set(3,1);
+        list.set(4,3);
+        list.set(5,5);
+        System.out.println(list);
+        
+        list.remove(5);
+        System.out.println(list);
+        
+        int len = list.size();
+        System.out.println(len);
+        
+        for(int i=0;i<list.size();i++) {
+            System.out.print(list.get(i)+" ");
+        }
+        System.out.println();
+        
+        Collections.sort(list);
+        System.out.println(list);
+        
+    }
+}
